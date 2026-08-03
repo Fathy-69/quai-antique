d4346d09d641090f77f2d23b8ea78c79519700b6
Fathy-69 <ftounekt@gmail.com>
2026-08-03 23:06:31 +0200

    Ajout d'un script JavaScript pour enrichir les fonctionnalités de l'application.

--- src/Views/layouts/footer.php
+++ src/Views/layouts/footer.php
@@ -73,5 +73,6 @@
     <!-- Bootstrap 5 JS Bundle & Custom JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="/assets/js/main.js"></script>
+    <script src="/assets/js/app.js"></script>
 </body>
 </html>
