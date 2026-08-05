6d9395d97badb0165c3a72e78cabb8589a3044ee
Fathy-69 <ftounekt@gmail.com>
2026-08-06 00:01:11 +0200

    Ajout de la gestion sécurisée des sessions avec des paramètres de cookie appropriés.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@
  * Architecture MVC
  */
 
+// Démarrage de session sécurisé
+session_set_cookie_params([
+    'httponly' => true,
+    'samesite' => 'Lax' // ou 'Strict'
+]);
+session_start();
+
 // Service direct des ressources statiques (CSS, JS, PNG, SVG)
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $filePath = __DIR__ . $uri;
